ea5a6cd0f07a5371b639474334327a1d0dbdfa97
harry <haribabbu.krishna@gmail.com>
2017-06-01 11:23:25 +0530

    Added The docker Support Currently HTTP Response Not Added

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 //Importing Standard Packages
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -25,7 +30,60 @@ func main() {
 	http.FileServer(http.Dir("views"))
 }*/
 func executecode(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "%s", ps.ByName("code"))
+	code := []byte(ps.ByName("code"))
+	err := ioutil.WriteFile("main.c", code, 777)
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	//var options types.CopyToContainerOptions
+
+	response, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "compiler",
+	}, nil, nil, "")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, response.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	content := bytes.NewReader(code)
+	if err := cli.CopyToContainer(ctx, response.ID, "/main.c", content, types.CopyToContainerOptions{}); err != nil {
+		panic(err)
+	}
+
+	exec_id, err := cli.ContainerExecCreate(ctx, response.ID, types.ExecConfig{
+		Cmd: []string{"gcc", "/main.c -o main"},
+	})
+	Use(exec_id)
+	if err != nil {
+		panic(err)
+	}
+	executable_id := "1"
+	if err := cli.ContainerExecStart(ctx, executable_id, types.ExecStartCheck{}); err != nil {
+		panic(err)
+	}
+
+	exect_id, err := cli.ContainerExecCreate(ctx, response.ID, types.ExecConfig{
+		Cmd: []string{"./main"},
+	})
+	Use(exect_id)
+	if err != nil {
+		panic(err)
+	}
+	if err := cli.ContainerExecStart(ctx, executable_id, types.ExecStartCheck{}); err != nil {
+		panic(err)
+	}
+
+	//fmt.Fprintf(w, "%s", ps.ByName("code"))
 }
 func showIndex(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	static_html, err := ioutil.ReadFile("views/index.html")
@@ -33,4 +91,25 @@ func showIndex(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		panic(err)
 	}
 	fmt.Fprintf(w, "%s", static_html)
+}
+func StopAllContainers() {
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		if err := cli.ContainerStop(ctx, container.ID, nil); err != nil {
+			panic(err)
+		}
+	}
+}
+func Use(something types.IDResponse) {
+
 }

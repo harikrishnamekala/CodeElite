33a694992b2d015f9ac9379c84fee8ffd62d28cf
harry <haribabbu.krishna@gmail.com>
2017-07-15 08:39:19 +0530

    To Clone the Directroies and Mount them to Containers

---
+++ controller/hostenvcreator.go
@@ -0,0 +1,9 @@
+package hostenvcreator
+
+/*
+To Create Clone Files of Source Code to Move the Code to the Container
+and Keep and Copy in the Host Env
+*/
+func CreateRespectiveEnvOfLanguage(language_id int, path string, defualt_template_env_path string) {
+
+}

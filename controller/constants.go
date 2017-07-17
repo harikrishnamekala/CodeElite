5038a785cb09060688d90d5a5b6f452058ad08e6
harry <haribabbu.krishna@gmail.com>
2017-07-17 21:58:17 +0530

    All The Constants in the App

---
+++ controller/constants.go
@@ -0,0 +1,6 @@
+package controller
+
+func ReturnContantLanguageid() []string  {
+  return language_id := []string{"CPP","GoLang","VisualBasic","php","CSharp","Perl","clojure","python",
+    "GCC","Shell","NodeJs","ruby"}
+}

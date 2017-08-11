5f99074d88401d421331a0f04285dac8c59051c6
harry <haribabbu.krishna@gmail.com>
2017-08-11 08:15:12 +0530

    Language File and Names Constants

--- controller/constants.go
+++ controller/constants.go
@@ -1,6 +1,26 @@
 package controller
 
-func ReturnContantLanguageid() []string  {
-  return language_id := []string{"CPP","GoLang","VisualBasic","php","CSharp","Perl","clojure","python",
-    "GCC","Shell","NodeJs","ruby"}
+func ReturnContantLanguageid() []string {
+	language_id_str := []string{"CPP", "GoLang", "VisualBasic", "php", "CSharp", "Perl", "clojure", "python",
+		"GCC", "Shell", "nodeJs", "ruby", "python3"}
+	return language_id_str
+}
+
+func ReturnFilesList(language_id int) []string {
+	data := map[int][]string{0: {"main.cpp", "input.txt", "errors.txt", "output.txt", "compile.sh"},
+		1:  {"main.go", "input.txt", "output.txt", "errors.txt", "compile.sh"},
+		2:  {"file.vb", "input.txt", "errors.txt", "output.txt", "compile.sh"},
+		3:  {"file.php", "input.txt", "output.txt", "compile.sh", "errors.txt"},
+		4:  {"Solution.cs", "input.txt", "errors.txt", "output.txt", "compile.sh"},
+		5:  {"file.pl", "input.txt", "output.txt", "compile.sh", "errors.txt"},
+		6:  {"file.clj", "input.txt", "output.txt", "compile.sh", "errors.txt"},
+		7:  {"file.py", "input.txt", "output.txt", "compile.sh", "errors.txt"},
+		8:  {"main.c", "input.txt", "errors.txt", "output.txt", "compile.sh"},
+		9:  {"script.sh", "input.txt", "output.txt", "compile.sh", "errors.txt"},
+		10: {"script.js", "input.txt", "output.txt", "compile.sh", "errors.txt"},
+		11: {"file.rb", "input.txt", "output.txt", "compile.sh", "errors.txt"},
+		12: {"file.py", "input.txt", "output.txt", "compile.sh", "errors.txt"},
+	}
+
+	return data[language_id]
 }

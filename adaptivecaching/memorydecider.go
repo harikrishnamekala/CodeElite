3a7ad9262aea97780efca720cb467f7af0526a2b
Harikrishna Mekala <haribabbu.krishna@gmail.com>
2017-12-07 12:10:09 +0530

    I am not getting Terminal Time

--- adaptivecaching/memorydecider.go
+++ adaptivecaching/memorydecider.go
@@ -1,15 +1,5 @@
 package adaptivecaching
 
-import (
-	"fmt"
-	"syscall"
-)
-
 func Getusage() {
-	var rusage *syscall.Rusage
 
-	if err := syscall.Getrusage(2, rusage); err != nil {
-		panic(err)
-	}
-	fmt.Println(rusage)
-}ls
+}
